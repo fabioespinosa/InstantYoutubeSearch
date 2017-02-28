e09a9742c9d479ae0e23db7d883b01ba2686cbde
Fabio Espinosa <fa.espinosa10@uniandes.edu.co>
2017-02-27 19:19:09 -0500

    hot fix
    layout hot fix

--- src/index.js
+++ src/index.js
@@ -37,10 +37,12 @@ class App extends Component{
     return (
       <div>
         <SearchBar onSearchTermChange={videoSearch}/>
-        <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList 
-          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
-          videos={this.state.videos}/>
+        <div className="row">
+          <VideoDetail video={this.state.selectedVideo}/>
+          <VideoList 
+            onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+            videos={this.state.videos}/>
+        </div>
       </div>
     );
   }
